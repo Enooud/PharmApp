1e3f2a98b73906c59606c97f255395d1f302886a
Enooud <wilnock1@gmail.com>
2019-11-13 11:30:45 +0100

    fin cration compte

--- src/pages/inscript/inscript.ts
+++ src/pages/inscript/inscript.ts
@@ -12,6 +12,11 @@ export class InscriptPage {
 
   Nom: string = "";
   Prenom: string = "";
+  Datnais: string ="";
+  Tel : string = "";
+  Email : string = "";
+  Pass : string = "";
+  Cmpass : string = "";
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public toastCtrl: ToastController, public postPvdr: PostProviders) {
@@ -25,16 +30,46 @@ export class InscriptPage {
     console.log('fonction ok');
     if(this.Nom == ""){
       const toast = this.toastCtrl.create({
-        message: ' le Nom requis',
+        message: 'Nom requis',
         duration: 2500
       });
       toast.present();
       }else if(this.Prenom == ""){
         const toast = this.toastCtrl.create({
-          message: ' le Prenom requis',
+          message: 'Prenom requis',
           duration: 2500
         });
         toast.present();  
+      }else if(this.Datnais ==""){
+        const toast = this.toastCtrl.create({
+          message: 'Date de naissance requise',
+          duration: 2500
+        });
+        toast.present();  
+      }else if(this.Tel ==""){
+        const toast = this.toastCtrl.create({
+          message: 'Numero de télephone requis',
+          duration: 2500
+        });
+        toast.present();
+      }else if(this.Email ==""){
+        const toast = this.toastCtrl.create({
+          message: 'Email requis',
+          duration: 2500
+        });
+        toast.present();
+      }else if(this.Pass ==""){
+        const toast = this.toastCtrl.create({
+          message: 'Mot de passe requis',
+          duration: 2500
+        });
+        toast.present();
+      }else if(this.Pass!= this.Cmpass){
+        const toast = this.toastCtrl.create({
+          message: 'Erreur de correspondance du Mot de Passe',
+          duration: 2500
+        });
+        toast.present();
       }
     else{
 
@@ -42,9 +77,13 @@ export class InscriptPage {
 
           Nom: this.Nom,
           Prenom: this.Prenom,
+          Datnais: this.Datnais,
+          Tel: this.Tel,
+          Email :this.Email,
+          Pass: this.Pass,
+          Cmpass: this.Cmpass,
           req:'Add'
       };
-
       this.postPvdr.postData(body, 'user.php').subscribe((data)=>
       { 
         var alertpesan = data.msg;
@@ -52,18 +91,24 @@ export class InscriptPage {
           this.navCtrl.pop();
           const toast = this.toastCtrl.create({
             message: 'Enregistrement effectué',
-            duration: 3000
+            duration: 2500
           });
           toast.present();
         }else{
           const toast = this.toastCtrl.create({
             message: alertpesan,
-            duration: 3000
+            duration: 2500
           });
           toast.present();
           }
         }); 
-  
+        
+        console.log(this.Nom);
+        console.log(this.Prenom);
+        console.log(this.Datnais);
+        console.log(this.Tel);
+        console.log(this.Email);
+        console.log(this.Pass);
       }
 
     }
